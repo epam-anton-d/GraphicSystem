103d52701c43499cf7941777dcd524ba9985a843
Anton D <Anton_Demin@epam.com>
2016-12-25 01:32:06 +0400

    Draws a line

---
+++ GraphSystem2/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace GraphSystem2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.figureChanger = new System.Windows.Forms.ListBox();
+            this.cleanButton = new System.Windows.Forms.Button();
+            this.actionChanger = new System.Windows.Forms.ListBox();
+            this.starTops = new System.Windows.Forms.ListBox();
+            this.figureColor = new System.Windows.Forms.ListBox();
+            this.thickness = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // figureChanger
+            // 
+            this.figureChanger.FormattingEnabled = true;
+            this.figureChanger.Location = new System.Drawing.Point(12, 12);
+            this.figureChanger.Name = "figureChanger";
+            this.figureChanger.Size = new System.Drawing.Size(136, 134);
+            this.figureChanger.TabIndex = 0;
+            this.figureChanger.SelectedIndexChanged += new System.EventHandler(this.figureChanger_SelectedIndexChanged);
+            // 
+            // cleanButton
+            // 
+            this.cleanButton.Location = new System.Drawing.Point(13, 412);
+            this.cleanButton.Name = "cleanButton";
+            this.cleanButton.Size = new System.Drawing.Size(75, 23);
+            this.cleanButton.TabIndex = 1;
+            this.cleanButton.Text = "Очистить";
+            this.cleanButton.UseVisualStyleBackColor = true;
+            this.cleanButton.Click += new System.EventHandler(this.cleanButton_Click);
+            // 
+            // actionChanger
+            // 
+            this.actionChanger.FormattingEnabled = true;
+            this.actionChanger.Location = new System.Drawing.Point(12, 153);
+            this.actionChanger.Name = "actionChanger";
+            this.actionChanger.Size = new System.Drawing.Size(136, 56);
+            this.actionChanger.TabIndex = 2;
+            // 
+            // starTops
+            // 
+            this.starTops.FormattingEnabled = true;
+            this.starTops.Location = new System.Drawing.Point(12, 216);
+            this.starTops.Name = "starTops";
+            this.starTops.Size = new System.Drawing.Size(120, 69);
+            this.starTops.TabIndex = 3;
+            // 
+            // figureColor
+            // 
+            this.figureColor.FormattingEnabled = true;
+            this.figureColor.Location = new System.Drawing.Point(13, 292);
+            this.figureColor.Name = "figureColor";
+            this.figureColor.Size = new System.Drawing.Size(120, 43);
+            this.figureColor.TabIndex = 4;
+            this.figureColor.SelectedIndexChanged += new System.EventHandler(this.figureColor_SelectedIndexChanged);
+            // 
+            // thickness
+            // 
+            this.thickness.FormattingEnabled = true;
+            this.thickness.Location = new System.Drawing.Point(13, 342);
+            this.thickness.Name = "thickness";
+            this.thickness.Size = new System.Drawing.Size(120, 43);
+            this.thickness.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 429);
+            this.Controls.Add(this.thickness);
+            this.Controls.Add(this.figureColor);
+            this.Controls.Add(this.starTops);
+            this.Controls.Add(this.actionChanger);
+            this.Controls.Add(this.cleanButton);
+            this.Controls.Add(this.figureChanger);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox figureChanger;
+        private System.Windows.Forms.Button cleanButton;
+        private System.Windows.Forms.ListBox actionChanger;
+        private System.Windows.Forms.ListBox starTops;
+        private System.Windows.Forms.ListBox figureColor;
+        private System.Windows.Forms.ListBox thickness;
+    }
+}
+
---
+++ GraphSystem2/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace GraphSystem2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Point[] point;
+        int pointCounter;
+        Pen pen;
+        Graphics drawArea;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Графические системы. Вариант №1";
+            string[] figures = new string[] { "Линия", "Кривая Безье", "Равнобедренный треугольник", "Звезда" };
+            string[] actions = new string[] { "Нарисовать" };
+            figureChanger.Items.AddRange(figures);
+            actionChanger.Items.AddRange(actions);
+
+            for (int topCounter = 3; topCounter < 21; topCounter++)
+            {
+                starTops.Items.Add(topCounter);
+            }
+
+            point = new Point[3];
+            pointCounter = 0;
+            var colors = new Color[] { Color.Blue, Color.Red, Color.Green };
+            figureColor.Items.Clear();
+            foreach (var item in colors)
+            {
+                figureColor.Items.Add(item);
+            }
+            figureColor.Sorted = true;
+
+            for (int i = 1; i < 5; i++)
+            {
+                thickness.Items.Add(i);
+            }
+
+            figureChanger.SelectedIndex = 0;
+            actionChanger.SelectedIndex = 0;
+            starTops.SelectedIndex = 0;
+            figureColor.SelectedIndex = 0;
+            thickness.SelectedIndex = 0;
+        }
+
+        private void cleanButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void figureChanger_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (actionChanger.SelectedItem == "Нарисовать")
+            {
+                if (figureChanger.SelectedItem == "Линия")
+                {
+                    switch(pointCounter)
+                    {
+                        case 0:
+                            point[pointCounter] = new Point(e.X, e.Y);
+                            pointCounter++;
+                            return;
+                        case 1:
+                            point[pointCounter] = new Point(e.X, e.Y);
+                            pointCounter = 0;
+                            pen = new Pen((Color)figureColor.SelectedItem);
+                            ProcDrawLine(drawArea, pen, point[0].X, point[0].Y, point[1].X, point[1].Y);
+
+                            return;
+                    }
+                }
+            }
+        }
+
+        private void figureColor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (figureColor.Text == "Transparent")
+            {
+                return;
+            }
+        }
+
+        public void ProcDrawLine(Graphics drawArea, Pen pen, int x1, int y1, int x2, int y2)
+        {
+            drawArea = CreateGraphics();
+            drawArea.SmoothingMode = SmoothingMode.HighQuality;
+            int x, y, dx, dy, Sx = 0, Sy = 0;
+            int F = 0, Fx = 0, dFx = 0, Fy = 0, dFy = 0;
+            dx = x2 - x1;
+            dy = y2 - y1;
+            bool tr = true;
+            Sx = Math.Sign(dx); Sy = Math.Sign(dy);
+            if (Sx > 0)
+                dFx = dy;
+            else
+                dFx = -dy;
+            if (Sy > 0)
+                dFy = dx;
+            else
+                dFy = -dx;
+            x = x1;
+            y = y1;
+            F = 0;
+            if (Math.Abs(dx) >= Math.Abs(dy))
+            {
+                // угол наклона <= 45 градусов
+                do
+                {
+                    //Вывести пиксель с координатами х, у
+                    Draw(drawArea, pen, x, y);
+                    if (x == x2)
+                        break;
+                    Fx = F + dFx;
+                    F = Fx - dFy;
+                    x = x + Sx;
+                    if (Math.Abs(Fx) < Math.Abs(F))
+                        F = Fx;
+                    else
+                        y = y + Sy;
+                } while (tr);
+            }
+            else // угол наклона > 45 градусов 
+            {
+                do { //Вывести пиксель с координатами х, у
+                    Draw(drawArea, pen, x, y);
+                    if (y == y2)
+                        break;
+                    Fy = F + dFy;
+                    F = Fy - dFx;
+                    y = y + Sy;
+                    if (Math.Abs(Fy) < Math.Abs(F))
+                        F = Fy;
+                    else x = x + Sx;
+                } while (tr);
+            }
+        }
+
+        // Вывод точки (квадрата)
+        public void Draw(Graphics drawArea, Pen pen, int x, int y)
+        {
+            drawArea.DrawRectangle(pen, x, y, thickness.SelectedIndex + 1, thickness.SelectedIndex + 1);
+        }
+    }
+}
